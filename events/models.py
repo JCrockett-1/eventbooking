c3d6c324645fcda63e8688425814edaf537d36ba
James Crockett <cro21013@byui.edu>
2026-03-03 18:57:56 -0700

    added comments

--- events/models.py
+++ events/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+# This makes the Event class which incluces a title, location, date, and time. It also has a __str__ function which returns how the function object will be named (eg. Church, 10/28)
 class Event(models.Model):
     title = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
@@ -10,6 +11,7 @@ class Event(models.Model):
     def __str__(self):
         return f'{self.title}, {self.date}'
     
+# This makes the Registration class which includes a given name and email, along with an event. A similar __str__ function is included which returns the name of the object (eg. James, Church)
 class Registration(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
--- events/views.py
+++ events/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import redirect
 from .models import Event
 from .models import Registration
 
+# This renders the event_list page. It gets all of the events already published and sends that info the the html
 def event_list(request):
     events = Event.objects.all()
     return render(request, 'event_list.html', {'events': events})
 
+# This allows a user to register for an event, if the register button is pressed, as long as a name and email are provided for a specific event, a registration object is created. It will also redirect to the confirmation page
 def register_for_event(request, event_id):
     event = Event.objects.get(id=event_id)
     if request.method == "POST":
@@ -17,6 +19,7 @@ def register_for_event(request, event_id):
     else:
         return render(request, 'registration_page.html', {'event': event})
     
+# This renders the confirmatino page, it is simply given the registration id which has all of the details for the event registered for and the user going, it also includes a redirect back to the event list
 def confirm_details(request, registration_id):
     registration = Registration.objects.get(id=registration_id)
     if request.method == "POST":
